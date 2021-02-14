d9f5cb9ca720eb7249493e797542e74ed2a80fc3
eatprinceton <79033077+eatprinceton@users.noreply.github.com>
2021-02-13 22:58:49 -0500

    Add files via upload

---
+++ src/colors.js
@@ -0,0 +1,19 @@
+  
+export default {
+	darkest: '#272635',
+	accent: '#7389FF',
+	lightest: '#F8F8FF',
+	dark: '',
+	lightBlue: '#C5E0FF',
+	lightGreen: '#ccffcc',
+	lightPurple: '#ddccff',
+	deliveryTag: '#99ddff', // blue
+	mexicanTag: '#ffccf2', // pink
+	asianTag: '#fff0b3', // yellow
+	takeoutTag: '#ffbb99', // orange
+	yumTag: '#99ffdd', // cyan
+	$$$Tag: '',
+
+	black: '#000000',
+	white: '#FFFFFF'
+};
