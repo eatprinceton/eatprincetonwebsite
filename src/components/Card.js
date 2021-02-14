463102d2d3009e2dc2cfbed265132d8679b2c22c
eatprinceton <79033077+eatprinceton@users.noreply.github.com>
2021-02-13 22:59:59 -0500

    Add files via upload

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../style.css';
+
+import Colors from '../colors';
+
+const Card = (props) => {
+	return (
+		<a href={props.url} target="_blank">
+			<div className="my-container column-dir between cardContainer">
+				<div className="my-container center">
+					<img src={props.img} className="full-img cardImg" />
+				</div>
+
+				<h2>{props.title}</h2>
+				<p className="cardInfo">{props.text}</p>
+				{props.tags}
+			</div>
+		</a>
+	);
+};
+
+export default Card;
---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../style.css";
+import { Link } from "react-router-dom";
+
+import logo from "../images/princeton.jpg";
+import Colors from '../colors';
+
+class Header extends Component {
+  state = {
+    toggle: false,
+    top: true,
+  };
+  Toggle = () => {
+    this.setState({ toggle: !this.state.toggle });
+    console.log(this.state.toggle);
+  };
+  headerColor = () => {
+    return { backgroundColor: "#423f4e" };
+  };
+  //   logoColor = () => {
+  //     if (!this.state.top || this.state.toggle) {
+  //       return white;
+  //     } else {
+  //       return orange;
+  //     }
+  //   };
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const top = window.scrollY < 100 ? true : false;
+      this.setState({ top: top });
+    });
+  }
+  render() {
+    return (
+      <div className="header" style={this.headerColor()}>
+        <Navbar collapseOnSelect expand="lg" bg="" variant="dark">
+          <Link
+            to="/"
+            className="my-container logo"
+            onClick={() => window.scrollTo(0, 0)}
+          >
+            <img
+              alt="Jayson"
+              src={logo}
+			  className="d-inline-block align-top logo"
+			  style = {styles.logo}
+            />
+          </Link>
+          <Navbar.Toggle
+            onClick={this.Toggle}
+            aria-controls="responsive-navbar-nav"
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" />
+            <Nav>
+              <Link
+                to="/"
+                className="nav-link"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                Restaurants
+              </Link>
+              <Link
+                to="/"
+                className="nav-link"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                Retail
+              </Link>
+              <Link
+                to="/"
+                className="nav-link"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                About
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+const styles = {
+	nameLink: {
+		fontSize: '23px',
+		fontWeight: '500',
+		lineHeight: '22px',
+		color: Colors.lightest,
+		textDecoration: 'none',
+		textShadow: '1px 1px 20px #272635'
+	},
+	logo: {
+		height: '50px'
+	}
+};
+
+export default Header;
