f33e07565753e6e22052e02ddac4503ece256f4c
jayson-wu <67655954+jayson-wu@users.noreply.github.com>
2021-03-13 16:04:30 -0500

    colors + images

--- src/colors.js
+++ src/colors.js
@@ -8,11 +8,34 @@ export default {
 	lightGreen: '#ccffcc',
 	lightPurple: '#ddccff',
 	deliveryTag: '#99ddff', // blue
-	mexicanTag: '#ffccf2', // pink
+	//mexicanTag: '#ffccf2', // pink
+	dineInTag: '#ffb3c6',
 	asianTag: '#fff0b3', // yellow
 	takeoutTag: '#ffbb99', // orange
 	yumTag: '#99ffdd', // cyan
 	$$$Tag: '',
+	americanTag: '#ffc6b3',
+    chineseTag: '#ffd9b3',
+    japaneseTag: '#ffecb3',
+    koreanTag: '#ffffb3',
+    mexicanTag: '#ecffb3',
+    italianTag: '#d9ffb3',
+    mediterraneanTag: '#c6ffb3',
+    indianTag: '#b3ffb3',
+    thaiTag: '#b3ffc6',
+    frenchTag: '#b3ffd9',
+    guatemalanTag: '#b3ffec',
+    barTag: '#b3ffff',
+    barandgrillTag: '#b3ecff',
+    cafeTag: '#b3d9ff',
+    bakeryTag: '#b3c6ff',
+    pizzaTag: '#b3b3ff',
+    sandwichTag: '#c6b3ff',
+    icecreamTag: '#d9b3ff',
+    sweetsTag: '#ecb3ff',
+    juiceandsmoothiesTag: '#ffb3ff',
+	
+
 
 	black: '#000000',
 	white: '#FFFFFF'
